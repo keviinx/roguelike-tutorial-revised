282a5add68f1176d250ea20a15e2818d1121b7c7
Keviindran Ramachandran <keviinx@yahoo.com>
2023-12-20 11:49:32 +0100

    fix: fix indentation error

--- procgen.py
+++ procgen.py
@@ -31,14 +31,14 @@ class RectangularRoom:
         """Return the inner area of this room as a 2D array index."""
         return slice(self.x1 + 1, self.x2), slice(self.y1 + 1, self.y2)
     
-def intersects(self, other: RectangularRoom) -> bool:
-    """Return True if this room overlaps with another RectangularRoom."""
-    return(
-        self.x1 <= other.x2
-        and self.x2 >= other.x1
-        and self.y1 <= other.y2
-        and self.y2 >= other.y1
-    )
+    def intersects(self, other: RectangularRoom) -> bool:
+        """Return True if this room overlaps with another RectangularRoom."""
+        return(
+            self.x1 <= other.x2
+            and self.x2 >= other.x1
+            and self.y1 <= other.y2
+            and self.y2 >= other.y1
+        )
 
 
 def tunnel_between(start: Tuple[int, int], end: Tuple[int, int]) -> Iterator[Tuple[int, int]]:
